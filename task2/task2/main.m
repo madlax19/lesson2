c0fb8641c2821f67fbf20c89eacb4eba2ac95fbb
Anton Lookin <anton.lookin@gmail.com>
2015-10-13 19:07:31 +0300

    Homework task2 init

---
+++ task2/task2/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  task2
+//
+//  Created by Anton Lookin on 10/13/15.
+//  Copyright © 2015 geekhub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+	return 0;
+}
