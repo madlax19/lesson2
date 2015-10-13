a68fb1ab936dd241eff37a506a4a98b8c15e46cf
Anton Lookin <anton.lookin@gmail.com>
2015-10-13 18:52:10 +0300

    Task1 Empty Project

---
+++ task1/task1/main.m
@@ -0,0 +1,20 @@
+//
+//  main.m
+//  task1
+//
+//  Created by Anton Lookin on 10/13/15.
+//  Copyright © 2015 geekhub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+	// Task 1
+	
+	
+	// Task 2
+	
+	
+	// Task 3
+	return 0;
+}
