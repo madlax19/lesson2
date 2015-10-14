1107db41185ae01357d96099db93e7219accfebb
Elena <madlax19@mail.ru>
2015-10-14 18:46:32 +0300

    Initialization and output variables

--- task1/task1/main.m
+++ task1/task1/main.m
@@ -9,7 +9,72 @@
 #import <UIKit/UIKit.h>
 
 int main(int argc, char * argv[]) {
-	// Task 1
+    char aVar = 'a';
+    short int shortIntVar = 3;
+    unsigned short int unsignedShortIntVar = 2;
+    int intVar = -4;
+    unsigned int unsignedIntVar = 5;
+    long int longIntVar = 6876542;
+    unsigned long int uLIVar = 7909897;
+    long long int lLIVar = -8787843676454123;
+    unsigned long long int uLLIVar= 9908762514123;
+    float floatVar = 3.9f;
+    double doubleVar = 56.422;
+    long double longDoubleVar = 576.578;
+    id idVar = nil;
+    BOOL boolVar = YES;
+    
+    NSString *hello = @"Hello";
+    NSString *hello1 = [[NSString alloc] initWithString:hello];
+    NSNumber *twentyOne = [[NSNumber alloc] initWithInt:21];
+    NSArray *planetsArray = @[@"Mars", @"Jupiter", @"Saturn", @"Pluto"];
+    NSDictionary *satellitesDictionary = @{
+        @"Deimos, Phobos": @"Mars",
+        @"Europe, Callisto, Io": @"Jupiter",
+        @"Titanium, Ray, Dion": @"Saturn",
+        @"Hydra, Styx":@"Pluto"
+        };
+    NSLog(@"Значение переменной aVar = %c", aVar);
+    NSLog(@"Значение переменной shortIntVar = %hi",shortIntVar);
+    NSLog(@"Значение переменной unsignedShortIntVar = %hx",unsignedShortIntVar);
+    NSLog(@"Значение переменной intVar  = %i",intVar  );
+    NSLog(@"Значение переменной unsignedIntVar = %u",unsignedIntVar);
+    NSLog(@"Значение переменной longIntVar = %li", longIntVar);
+    NSLog(@"Значение переменной uLIVar = %lu",uLIVar);
+    NSLog(@"Значение переменной lLIVar = %lli",lLIVar);
+    NSLog(@"Значение переменной uLLIVar = %llu",uLLIVar );
+    NSLog(@"Значение переменной floatVar = %f",floatVar);
+    NSLog(@"Значение переменной doubleVar  = %f",doubleVar);
+    NSLog(@"Значение переменной longDoubleVar = %Lf",longDoubleVar);
+    NSLog(@"Значение переменной idVar = %p",idVar);
+    NSLog(@"Значение переменной boolVar = %hhd",boolVar);
+    
+    NSLog(@"Значение переменной hello: %@", hello);
+    NSLog(@"Значение переменной hello1: %@", hello1);
+    NSLog(@"Значение переменной twentyOne: %@ ", twentyOne);
+    NSLog(@"Значение переменной planetsArray: %@", planetsArray);
+    NSLog(@"Значение переменной satellitesDictionary: %@", satellitesDictionary);
+    
+    
+ 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 	
 	
 	// Task 2
